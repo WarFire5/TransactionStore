de1f387207d55640491d9d7e2e47d59d4c17e088
Myslyaev Vasiliy <myslyaev@gmail.com>
2024-06-02 21:34:27 +0300

    UPD Tansaction controller, upd routes naming

--- TransactionStore.API/Controllers/AccountsController.cs
+++ TransactionStore.API/Controllers/AccountsController.cs
@@ -24,7 +24,7 @@ public class AccountsController : Controller
         return Ok(_transactionsService.GetBalanceByAccountId(id));
     }
 
-    [HttpGet("/transactions/{id}")]
+    [HttpGet("{id}/transactions")]
     public ActionResult<List<TransactionResponse>> GetTransactionsByAccountId(Guid id)
     {
         _logger.Information($"Getting the account transactions by its Id {id}. / Получаем транзакции аккаунта по его Id {id}.");
