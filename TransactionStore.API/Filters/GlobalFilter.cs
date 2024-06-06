0b36df5c4ab12b94a5735e513b63d806db635367
Myslyaev Vasiliy <myslyaev@gmail.com>
2024-06-07 02:04:02 +0300

    Add Global Filter and 403 Exception

--- TransactionStore.API/Filters/GlobalFilter.cs
+++ TransactionStore.API/Filters/GlobalFilter.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Serilog;
+using TransactionStore.Core.Exceptions;
+using ILogger = Serilog.ILogger;
 
 namespace TransactionStore.API.Filters;
 
 public class GlobalFilter : IActionFilter
 {
+    private readonly ILogger _logger = Log.ForContext<GlobalFilter>();
     public void OnActionExecuted(ActionExecutedContext context)
     {
-        //здесь будет фильтрация по адресу нужного API
+        if (Convert.ToString(context.HttpContext.Request.Host) != "194.87.210.5:10000")
+        {
+            _logger.Debug($"Access from adress {context.HttpContext.Request.Host} denied.");
+            throw new ForbiddenException();
+        }
     }
 
     public void OnActionExecuting(ActionExecutingContext context)
---
+++ TransactionStore.Core/Exceptions/ForbiddenException.cs
@@ -0,0 +1,8 @@
+﻿namespace TransactionStore.Core.Exceptions;
+
+public class ForbiddenException : Exception
+{
+    public ForbiddenException() : base("Access denied. / Доступ запрещен.") { }
+
+    public ForbiddenException(string message) : base(message) { }
+}
