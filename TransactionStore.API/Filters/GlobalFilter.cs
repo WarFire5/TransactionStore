cf1ddadb744e1828d556506c13eead2b9f2a93bb
Myslyaev Vasiliy <myslyaev@gmail.com>
2024-06-10 16:23:10 +0300

    Comment Filter

--- TransactionStore.API/Filters/GlobalFilter.cs
+++ TransactionStore.API/Filters/GlobalFilter.cs
@@ -11,11 +11,11 @@ public class GlobalFilter : IActionFilter
     private readonly ILogger _logger = Log.ForContext<GlobalFilter>();
     public void OnActionExecuted(ActionExecutedContext context)
     {
-        if (context.HttpContext.Request.Host.ToString() != ConfigurationSettings.CrmApiAdress)
-        {
-            _logger.Debug($"Access from adress {context.HttpContext.Request.Host} denied.");
-            throw new ForbiddenException();
-        }
+        //if (context.HttpContext.Request.Host.ToString() != ConfigurationSettings.CrmApiAdress)
+        //{
+        //    _logger.Debug($"Access from adress {context.HttpContext.Request.Host} denied.");
+        //    throw new ForbiddenException();
+        //}
     }
 
     public void OnActionExecuting(ActionExecutingContext context)
