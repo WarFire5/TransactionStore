e689f48d8ec7a96d15a54fe6b46c373a17db8ef9
WarFire5 <tatyanamirvoda@yandex.ru>
2024-06-15 21:02:05 +0300

    Added tests for CommissionsProvider

--- TransactionStore.Core/Data/CommissionsProvider.cs
+++ TransactionStore.Core/Data/CommissionsProvider.cs
@@ -1,8 +1,12 @@
-﻿namespace TransactionStore.Core.Data;
+﻿using Serilog;
+using TransactionStore.Core.Enums;
+
+namespace TransactionStore.Core.Data;
 
 public class CommissionsProvider : ICommissionsProvider
 {
     private readonly Dictionary<string, decimal> _percent;
+    private readonly ILogger _logger = Log.ForContext<CommissionsProvider>();
 
     public CommissionsProvider()
     {
@@ -24,8 +28,10 @@ public class CommissionsProvider : ICommissionsProvider
         var transaction = ConvertTransactionEnumToString(transactionType);
         if (_percent.TryGetValue(transaction, out var percent))
         {
+            _logger.Information($"Returning percent of commission за {transactionType} - {percent}. / Возврат процента комиссии за {transactionType} – {percent}");
             return percent;
         }
+        _logger.Error($"Throwing an error if commission percentage for transaction of type {transaction} not found. / Выдача ошибки, если процент комиссии для транзакции типа {transaction} не найден.");
         throw new ArgumentException($"The commission percentage for transaction of type {transaction} not found. / Процент комиссии для транзакции типа {transaction} не найден.");
     }
 }
